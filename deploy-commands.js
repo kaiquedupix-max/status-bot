1f1648dd63c363e9cc2088d528cdc3d97ead3128
kaiquedupix-max <kaiquedupix@gmail.com>
2026-07-29 10:17:30 -0300

    Criar deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+
+const {
+    REST,
+    Routes
+} = require("discord.js");
+
+const {
+    commands
+} = require("./commands");
+
+
+const rest = new REST({
+
+    version:"10"
+
+}).setToken(
+
+    process.env.DISCORD_TOKEN
+
+);
+
+
+
+async function deploy(){
+
+
+    try{
+
+
+        console.log("🔄 Registrando comandos...");
+
+
+        await rest.put(
+
+            Routes.applicationGuildCommands(
+
+                process.env.CLIENT_ID,
+
+                process.env.GUILD_ID
+
+            ),
+
+            {
+
+                body: commands.map(
+                    cmd=>cmd.toJSON()
+                )
+
+            }
+
+        );
+
+
+
+        console.log(
+            "✅ Comandos registrados!"
+        );
+
+
+
+    }catch(error){
+
+
+        console.log(
+            "❌ Erro registrando comandos:"
+        );
+
+
+        console.log(error);
+
+
+    }
+
+
+}
+
+
+
+deploy();
